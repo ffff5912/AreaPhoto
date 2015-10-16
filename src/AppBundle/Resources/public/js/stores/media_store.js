00377d60e7ffe91f2ce1a30b2130562b963df59b
ffff5912 <iioka5912@gmail.com>
2015-10-16 21:50:28 +0900

    change request parameter
    FormBuilder向けにパラメータを変更

--- src/AppBundle/Resources/public/js/stores/media_store.js
+++ src/AppBundle/Resources/public/js/stores/media_store.js
@@ -7,11 +7,15 @@ var MediaStore = (function() {
 
     MediaStore.prototype.find = function(data) {
         var self = this;
+        data.query = {
+            "appbundle_location[lat]": data.latLng.lat(),
+            "appbundle_location[lng]": data.latLng.lng(),
+        };
         $.ajax({
             url: self.url,
             type: 'GET',
             beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', self.token);},
-            data: {lat: data.latLng.lat(), lng: data.latLng.lng()},
+            data: data.query,
             success: function(response) {
                 data.callback(response);
             }
