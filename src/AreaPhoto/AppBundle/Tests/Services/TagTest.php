53fdd5d5597a429be05f41e15637441139c9e5c8
ffff5912 <iioka5912@gmail.com>
2015-12-08 23:47:18 +0900

    add tests

--- src/AreaPhoto/AppBundle/Tests/Services/TagTest.php
+++ src/AreaPhoto/AppBundle/Tests/Services/TagTest.php
@@ -9,6 +9,11 @@ class TagTest extends WebTestCase
 {
     protected static $hash_tag_schema;
 
+    private $end_points = [
+        'recent' => 'https://api.instagram.com/v1/tags/%s/media/recent',
+        'search' => 'https://api.instagram.com/v1/tags/search'
+    ];
+
     public static function setUpBeforeClass()
     {
         self::$hash_tag_schema = [
@@ -26,6 +31,17 @@ class TagTest extends WebTestCase
 
     public function testSearchSuccess()
     {
+        $provider = $this->getMockBuilder('AreaPhoto\AppBundle\Providers\ProviderInterface')->disableOriginalConstructor()->getMock();
+        $provider->expects($this->once())
+            ->method('get')
+            ->will(sprintf($this->end_points['recent'], '東京'))
+            ->with($this->returnValue());
+        $cache = $this->getMockBuilder('AreaPhoto\AppBundle\Storage\Cache\CacheManager')->disableOriginalConstructor()->getMock();
+
+        $service = new Tag($provider, $this->end_points, $cache);
+        $media = $service->execute('東京');
+
+
         $tag = $this->service->search('東京');
         $this->assertCount(0, array_diff_key($tag[0], array_flip(self::$hash_tag_schema)));
 
